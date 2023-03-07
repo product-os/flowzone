06d06882a78d679c7fb2a66d5f620ae478398e34
Kyle Harding <kyle@balena.io>
2023-03-07 16:15:24 -0500

    Run some simple benchmarks via docker
    Change-type: patch
    Signed-off-by: Kyle Harding <kyle@balena.io>

--- tests/entrypoint.sh
+++ tests/entrypoint.sh
@@ -5,3 +5,7 @@ then
 	echo "REPO_SECRET value is incorrect"
 	exit 1
 fi
+
+sysbench --test=cpu --cpu-max-prime=20000 run
+sysbench --test=memory run
+sysbench --test=fileio --file-test-mode=seqwr run
